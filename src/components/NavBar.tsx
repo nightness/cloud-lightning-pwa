7b1cf33f96fb4f5adaa770390d926afe3b00113e
nightness <joshguyette@yahoo.com>
2021-06-18 00:39:23 -0500

    snapshot

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import Text from './Text'
 import './NavBar.css'
 
 const Navbar = () => {
@@ -6,14 +7,17 @@ const Navbar = () => {
 
     const currentLocationStyle = {
         color: 'white',
-        backgroundColor: '#f1356d',
+        backgroundColor: '#0352fc',
         borderRadius: '5px',
-        padding: 0
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10
     }
 
     return (
         <nav className="navbar">
-            <h1>Welcome to Cloud-Lightning PWA</h1>
+            <Text headerSize={1}>Welcome to Cloud-Lightning PWA</Text>
             <div className="links">
                 <Link to="/" style={location.pathname === '/' ? currentLocationStyle : undefined}>Home</Link>
                 <Link to="/webRTC" style={location.pathname === '/webRTC' ? currentLocationStyle : undefined}>WebRTC</Link>
