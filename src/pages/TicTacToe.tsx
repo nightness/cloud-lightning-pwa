178d0b2590d520c1094199e593543a6cf71217a5
nightness <joshguyette@yahoo.com>
2021-07-10 14:30:19 -0500

    computer can now auto-play winning and blocking moves

--- src/pages/TicTacToe.tsx
+++ src/pages/TicTacToe.tsx
@@ -50,13 +50,13 @@ export default () => {
   // For the computer's usage, to block player
   // Return the unused index if only one of [(a === b) || (b === c) || (c === a)]
   // are true, and the other index is blank
-  const eval2 = (a: string, b: string, c: string) => {
+  const eval2 = (a: string, b: string, c: string, symbol = playerIs) => {
     const results = !eval3(a, b, c);
-    if (results && a === playerIs && a === b && c === "") {
-      return 3;
-    } else if (results && b === playerIs && b === c && a === "") {
+    if (results && a === symbol && a === b && c === "") {
+      return 2;
+    } else if (results && b === symbol && b === c && a === "") {
       return 0;
-    } else if (results && c === playerIs && c === a && b === "") {
+    } else if (results && c === symbol && c === a && b === "") {
       return 1;
     }
     return false;
@@ -76,17 +76,69 @@ export default () => {
   };
 
   const doComputersMove = () => {
+    if (playerIs === turn) return;
+    const mySymbol = turn === "O" ? "O" : "X";
+    // For the win
     const results = {
-      row1: eval2(board[0][0], board[0][1], board[0][2]),
-      row2: eval2(board[1][0], board[1][1], board[1][2]),
-      row3: eval2(board[2][0], board[2][1], board[2][2]),
-      col1: eval2(board[0][0], board[1][0], board[2][0]),
-      col2: eval2(board[0][1], board[1][1], board[2][1]),
-      col3: eval2(board[0][2], board[1][2], board[2][2]),
-      d1: eval2(board[0][0], board[1][1], board[2][2]),
-      d2: eval2(board[0][2], board[1][1], board[2][0]),
+      row1: eval2(board[0][0], board[0][1], board[0][2], mySymbol),
+      row2: eval2(board[1][0], board[1][1], board[1][2], mySymbol),
+      row3: eval2(board[2][0], board[2][1], board[2][2], mySymbol),
+      col1: eval2(board[0][0], board[1][0], board[2][0], mySymbol),
+      col2: eval2(board[0][1], board[1][1], board[2][1], mySymbol),
+      col3: eval2(board[0][2], board[1][2], board[2][2], mySymbol),
+      d1: eval2(board[0][0], board[1][1], board[2][2], mySymbol),
+      d2: eval2(board[0][2], board[1][1], board[2][0], mySymbol),
     };
+    const newBoard = createNewBoard(board);
+    console.assert(newBoard !== undefined);
+    if (results.row1 !== false) newBoard[0][results.row1 as number] = mySymbol;
+    else if (results.row2 !== false)
+      newBoard[1][results.row2 as number] = mySymbol;
+    else if (results.row3 !== false)
+      newBoard[2][results.row3 as number] = mySymbol;
+    else if (results.col1 !== false)
+      newBoard[results.col1 as number][0] = mySymbol;
+    else if (results.col2 !== false)
+      newBoard[results.col2 as number][1] = mySymbol;
+    else if (results.col3 !== false)
+      newBoard[results.col3 as number][2] = mySymbol;
+    else if (newBoard[1][1] === "") newBoard[1][1] = mySymbol;
+    else {
+      // For the block
+      const results = {
+        row1: eval2(board[0][0], board[0][1], board[0][2]),
+        row2: eval2(board[1][0], board[1][1], board[1][2]),
+        row3: eval2(board[2][0], board[2][1], board[2][2]),
+        col1: eval2(board[0][0], board[1][0], board[2][0]),
+        col2: eval2(board[0][1], board[1][1], board[2][1]),
+        col3: eval2(board[0][2], board[1][2], board[2][2]),
+        d1: eval2(board[0][0], board[1][1], board[2][2]),
+        d2: eval2(board[0][2], board[1][1], board[2][0]),
+      };
+      console.log(results);
+      if (results.row1 !== false)
+        newBoard[0][results.row1 as number] = mySymbol;
+      else if (results.row2 !== false)
+        newBoard[1][results.row2 as number] = mySymbol;
+      else if (results.row3 !== false)
+        newBoard[2][results.row3 as number] = mySymbol;
+      else if (results.col1 !== false)
+        newBoard[results.col1 as number][0] = mySymbol;
+      else if (results.col2 !== false)
+        newBoard[results.col2 as number][1] = mySymbol;
+      else if (results.col3 !== false)
+        newBoard[results.col3 as number][2] = mySymbol;
+      else if (newBoard[1][1] === "") newBoard[1][1] = mySymbol;
+      else return;
+    }
     console.log(results);
+    setBoard(newBoard);
+    setTurn(turn === "X" ? "O" : "X");
+    setMessage(
+      turn === playerIs
+        ? "Computer's Turn"
+        : `Player's Turn (player is '${playerIs}')`
+    );
   };
 
   useEffect(() => {
