b9fbe7479c316e391ce51c3c76ff63cfb7c81958
nightness <joshguyette@yahoo.com>
2021-07-10 09:52:44 -0500

    fixed game over logic

--- src/pages/TicTacToe.tsx
+++ src/pages/TicTacToe.tsx
@@ -8,45 +8,78 @@ type Board = [
   [string, string, string]
 ];
 
-const createNewBoard = () =>
-  [
+const createNewBoard = (existing?: Board) => {
+  const result = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
   ] as Board;
 
+  if (!existing) return result;
+
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      result[r][c] = existing[r][c];
+    }
+  }
+
+  return result
+}
+
 export default () => {
   const [isStarted, setIsStarted] = useState(false);
-  const [gameOver, setGameOver] = useState(true)
+  const [gameOver, setGameOver] = useState(true);
   const [board, setBoard] = useState(createNewBoard());
-  const [turn, setTurn] = useState<'X' | 'O'>() 
+  const [turn, setTurn] = useState<"X" | "O">();
 
   const onClickAnyCell = (row: number, col: number) => {
     if (!isStarted || !turn || gameOver) return;
-    const newBoard = createNewBoard();
-    for (let r = 0; r < 3; r++) {
-      for (let c = 0; c < 3; c++) {
-        newBoard[r][c] = board[r][c];
-      }
-    }
+    const newBoard = createNewBoard(board);
     newBoard[row][col] = turn;
     setBoard(newBoard);
-    setTurn(turn === 'X' ? 'O' : 'X')
+    setTurn(turn === "X" ? "O" : "X");
   };
 
+  const eval3 = (a: string, b: string, c: string) =>  a && a === b && b === c && a ===c;
+
   useEffect(() => {
-    const row1 = board[0][0] !== '' && (board[0][0] === board[0][1]) && (board[0][1] === board[0][2])
-    const row2 = board[1][0] !== '' && (board[1][0] === board[1][1]) && (board[1][1] === board[1][2])
-    const row3 = board[2][0] !== '' && (board[2][0] === board[2][1]) && (board[2][1] === board[2][2])
-    const col1 = board[0][0] !== '' && (board[0][0] === board[1][0]) && (board[1][0] === board[2][0])
-    const col2 = board[0][1] !== '' && (board[0][1] === board[1][1]) && (board[1][1] === board[2][1])
-    const col3 = board[0][2] !== '' && (board[0][2] === board[1][2]) && (board[1][2] === board[2][2])
-    const d1 = board[0][0] !== '' && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])
-    const d2 = board[0][2] !== '' && (board[0][2] === board[1][1]) && (board[1][1] === board[0][2])
-    if (row1 || row2 || row3 || col1 || col2 || col3 || d1 || d2) {
-      setGameOver(true);
-    }      
-  }, [board])
+    let newGameOver = gameOver;
+    const results =  {
+      row1: eval3(board[0][0], board[0][1], board[0][2]),
+      row2: eval3(board[1][0], board[1][1], board[1][2]),
+      row3: eval3(board[2][0], board[2][1], board[2][2]),
+
+      col1: eval3(board[0][0], board[1][0], board[2][0]),
+      col2: eval3(board[0][1], board[1][1], board[2][1]),
+      col3: eval3(board[0][2], board[1][2], board[2][2]),
+
+      d1: eval3(board[0][0], board[1][1], board[2][2]),
+      d2: eval3(board[2][0], board[1][1], board[2][2]),
+      filled:
+        board[0][0] &&
+        board[0][1] &&
+        board[0][2] &&
+        board[1][0] &&
+        board[1][1] &&
+        board[1][2] &&
+        board[2][0] &&
+        board[2][1] &&
+        board[2][2]
+    }
+    //console.log(results, board)
+    setGameOver(
+      newGameOver ||
+        !!results.row1 ||
+        !!results.row2 ||
+        !!results.row3 ||
+        !!results.col1 ||
+        !!results.col2 ||
+        !!results.col3 ||
+        !!results.d1 ||
+        !!results.d2 ||
+        !!results.filled
+    );
+  }, [board]);
 
   return (
     <Page
@@ -108,10 +141,10 @@ export default () => {
           title="Start"
           style={{ marginTop: 30, width: "25%" }}
           onPress={() => {
-            setBoard(createNewBoard())
+            setBoard(createNewBoard());
             setGameOver(false);
             setIsStarted(true);
-            setTurn(Math.random() > 0.5 ? 'X' : 'O')
+            setTurn(Math.random() > 0.5 ? "X" : "O");
           }}
         />
       )}
