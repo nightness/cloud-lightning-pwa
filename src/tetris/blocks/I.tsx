b95119bccc244813ecc2efd0d43dc99306fa8efe
nightness <joshguyette@yahoo.com>
2021-07-20 19:20:02 -0500

    Fixed improper display

--- src/tetris/blocks/I.tsx
+++ src/tetris/blocks/I.tsx
@@ -1,17 +1,37 @@
-import { BlockProps } from "./Block";
+import { BlockProps } from "..";
 
 export default ({ style, orientation }: BlockProps) => {
+  let outerClass = "flex-column";
+  let innerClass = "flex-row";
+  switch (orientation) {
+    case 90:
+      outerClass = "flex-row-reverse";
+      innerClass = "flex-column";
+      break;
+    case 180:
+      outerClass = "flex-column-reverse";
+      innerClass = "flex-row-reverse";
+      break;
+    case 270:
+      outerClass = "flex-row";
+      innerClass = "flex-column-reverse";
+      break;
+  }
+
   return (
-    <div
-      style={style}
-      className={`${
-        orientation === 0 || orientation == 180 ? "flex-column" : "flex-row"
-      }`}
-    >
-      <div className="box" />
-      <div className="box" />
-      <div className="box" />
-      <div className="box" />
+    <div style={style} className={outerClass}>
+      <div className={innerClass}>
+        <div className="blank" />
+        <div className="blank" />
+        <div className="blank" />
+        <div className="blank" />
+      </div>
+      <div className={innerClass}>
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
     </div>
   );
 }
