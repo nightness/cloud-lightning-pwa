4d4bba91c39b7dea8447bf959ee3723e393b60b4
nightness <joshguyette@yahoo.com>
2021-07-20 21:03:29 -0500

    Noughts and Crosses mets Tetris

--- src/tetris/index.ts
+++ src/tetris/index.ts
@@ -6,13 +6,13 @@ import O from './blocks/O'
 import S from './blocks/S'
 import T from './blocks/T'
 import Z from './blocks/Z'
-import X from './blocks/X'
+import XO from './blocks/XO'
 import TestPanel from "./TestPanel";
 import Tetris from "./TetrisPage";
 import TetrisBoard from "./TetrisBoard";
 import Block, { BlockProps, BlockTypes, OrientationValue } from "./blocks/Block"
 
 export { Block, Tetris, TetrisBoard, TestPanel };
-export { I, J, L, O, S, T, Z, X }
+export { I, J, L, O, S, T, Z, XO }
 
 export type { BlockProps, BlockTypes, OrientationValue }
