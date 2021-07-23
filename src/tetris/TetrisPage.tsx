e1dd145c47f265fd56de25e9c7657483329fa457
nightness <joshguyette@yahoo.com>
2021-07-23 18:45:28 -0500

    refactoring

--- src/tetris/TetrisPage.tsx
+++ src/tetris/TetrisPage.tsx
@@ -9,6 +9,7 @@ export default function Tetris() {
         <div>
           <h3>Use of a physical keyboard is the only way to interact atm</h3>
           <h4>The R key will reset the the game</h4>
+          <h4>The P key will pause the game</h4>
           <h4>Change piece with the following keys: I, J, L, O, S, T, X, Z</h4>
           <h4>Rotate piece with SpaceBar</h4>
           <h4>Move the piece with: Left (-1x), Right (1x), Up (top), Down (bottom) to move the piece</h4>
