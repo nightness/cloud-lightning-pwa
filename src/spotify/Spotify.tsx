b4de6465109982fc361891c3dfc0f9dd1ececace
nightness <joshguyette@yahoo.com>
2021-07-29 15:38:43 -0500

    snapshot

--- src/spotify/Spotify.tsx
+++ src/spotify/Spotify.tsx
@@ -3,6 +3,7 @@ import { Button, Page, TextInput } from "../components";
 import { FirebaseContext } from "../database/FirebaseContext";
 import { useDocument } from "../database/Firebase";
 import { SpotifyContext, SpotifyFirebaseData } from "./SpotifyContext";
+import SpotifyPlayer from "react-spotify-web-playback";
 export const redirectUri = encodeURI("http://localhost:3000/home/spotify");
 
 export default function Spotify() {
@@ -11,7 +12,11 @@ export default function Spotify() {
   const [doc, loading, error] = useDocument(`private/${currentUser?.uid}`);
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [searchText, setSearchText] = useState("");
-  const [searchResults, setSearchResults] = useState<SpotifyApi.TrackObjectFull[]>([])
+  const [play, setPlay] = useState(false);
+  const [trackUri, setTrackUri] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState<
+    SpotifyApi.TrackObjectFull[]
+  >([]);
 
   const parameters = new URLSearchParams(window.location.hash.substr(1));
   const accessTokenParam = parameters.get("access_token");
@@ -20,16 +25,20 @@ export default function Spotify() {
   useEffect(() => {
     if (!doc || loading || error) return;
     const data = doc.data() as SpotifyFirebaseData;
-    if (data && data.accessToken && data.expiresAt && data.expiresAt > Date.now()) {
+    const remainingTime = data.expiresAt ? data.expiresAt - Date.now() : 0;
+    //console.log("Test: ", data.accessToken, data?.expiresAt, Date.now(), remainingTime);
+    if (data && data.accessToken && remainingTime > 10000) {
       spotify.setAccessToken(data.accessToken);
       setAccessToken(data.accessToken);
+    } else {
+      spotify.authorize();
     }
   }, [doc, loading, error]);
 
   useEffect(() => {
     if (accessTokenParam && expiresInParam && doc && currentUser) {
       const docRef = doc.ref;
-      const expireTime = Date.now() + parseInt(expiresInParam);      
+      const expireTime = Date.now() + parseInt(expiresInParam) * 1000;
       docRef
         .set({
           accessToken: accessTokenParam,
@@ -47,16 +56,23 @@ export default function Spotify() {
       return;
     }
     let cancel = false;
-    spotify.api.searchTracks(searchText).then((response) => {
-      if (cancel) return;
-      setSearchResults(response.body.tracks?.items || [])
-    })
-    return () => { cancel = true };
-  }, [searchText, accessToken])
+    spotify.api
+      .searchTracks(searchText)
+      .then((response) => {
+        if (cancel) return;
+        setSearchResults(response.body.tracks?.items || []);
+      })
+      .catch(() => {
+        spotify.authorize();
+      });
+    return () => {
+      cancel = true;
+    };
+  }, [searchText, accessToken]);
 
   useEffect(() => {
-    console.log(searchResults)
-  }, [searchResults])
+    console.log(searchResults);
+  }, [searchResults]);
 
   if (accessTokenParam) return <h3>Signing-in...</h3>;
 
@@ -65,12 +81,59 @@ export default function Spotify() {
       {!accessToken ? (
         <Button text="Login" onClick={() => spotify.authorize()} />
       ) : (
-        <div className="flex-column">
+        <div className="flex-column test">
           <div>
-            <TextInput value={searchText} onChangeValue={setSearchText} placeholder='Search Songs/Albums' />
+            <TextInput
+              value={searchText}
+              onChangeValue={setSearchText}
+              placeholder="Search Songs/Albums"
+            />
             <Button text="Search" onClick={() => console.log(searchResults)} />
           </div>
-          <Button text="Test" onClick={() => spotify.test()} />
+          <div style={{ position: "relative", width: "100%" }}>
+            {searchResults.map((track, index, array) => (
+              <div
+                className=""
+                style={{
+                  padding: "5px",
+                  display: "grid",
+                  gridTemplateColumns: "64px 45% 45%",
+                  width: "100%",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  console.log("click", track.uri);
+                  setTrackUri(track.uri);
+                  setPlay(true);
+                }}
+                key={`${Math.random()}`}
+              >
+                <img
+                  src={track.album.images[0].url}
+                  style={{ width: 64, height: 64 }}
+                />
+                <div className="flex-column" style={{ justifyContent: 'center' }}>
+                  <h3>{track.artists[0].name}</h3>
+                  {track.album.name === track.artists[0].name ? (
+                    <></>
+                  ) : (
+                    <h4>{track.album.name}</h4>
+                  )}
+                </div>
+                <h2>{track.name}</h2>
+              </div>
+            ))}
+          </div>
+          {/* <Button text="Test" onClick={() => spotify.test()} /> */}
+          <SpotifyPlayer
+            token={accessToken}
+            showSaveIcon
+            callback={(state) => {
+              if (!state.isPlaying) setPlay(false);
+            }}
+            play={play}
+            uris={trackUri ? [trackUri] : []}
+          />
         </div>
       )}
     </Page>
