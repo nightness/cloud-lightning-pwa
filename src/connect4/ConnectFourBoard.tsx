9908c72a52198da39a16ae3bf8d0df7395da48a9
nightness <joshguyette@yahoo.com>
2021-08-04 10:57:53 -0500

    initial connect 4 commit

--- src/connect4/ConnectFourBoard.tsx
+++ src/connect4/ConnectFourBoard.tsx
@@ -3,12 +3,20 @@ import React from 'react';
 type Player = 1 | 2;
 type BoardValue = 0 | 1 | 2;
 
-// Returns [Row, Column] in a tuple of the 
+type FindWinner = (board: BoardValue[][]) => Player | void | undefined
+
+const findWinner: FindWinner = (board: BoardValue[][]) => {
+    
+}
+
+// Returns [Row, Column] in a tuple of the, if row and column are negative, their is
+// no move and the game is over.
 type ComputerMove = (board: BoardValue[][], maximizeFor: Player) => [number, number]
 
 const computerMove: ComputerMove = (board, maximizeFor) => {
     let row = -1;
     let column = -1;
+
     return [row, column]
 }
 
