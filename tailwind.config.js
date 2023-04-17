832e21635cfc85a74d8c00324ce2c863e870483a
Josh Guyette <joshguyette@yahoo.com>
2023-04-17 16:38:04 -0500

    adding tailwind-elements

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
